9b05441ada46083276210c1c2fc987d2d2f45903
ubuciko <info@ubuciko.de>
2012-05-06 13:47:19 +0200

    Text zur Presseakkreditierung hinzugefügt

--- document_root/presse.php
+++ document_root/presse.php
@@ -23,6 +23,15 @@
 				<li><a href="impressum.php">Impressum &amp; Kontakt</a></li>
 			</ul>
 			<div id="content">
+				<h2>Presseakkreditierung zum Landesparteitag 2012.1 der PIRATEN Th&uuml;ringen</h2>
+				<p>Sehr geehrte Pressevertreter,</p>
+				<p>wir laden Sie herzlich zum Landesparteitag 2012.1 der PIRATEN Th&uuml;ringen am 12./13. Mai in das B&uuml;rgerhaus, Seestra&szlig;e 18, 99094 Erfurt-Schmira ein.</p>
+				<p><strong>Die Presseakkreditierung findet an beiden Tagen ab 9.00 Uhr statt.</strong></p>
+				<p>Die Pressemappe zum Landesparteitag finden Sie hier als Download:</p>
+				<p class="extern">
+					<a href="http://wiki.piraten-thueringen.de/images/0/08/Pressemappe_Final.pdf"><img src="pdf.png" />Pressemappe</a>
+				</p><br />
+				<p>Haben Sie noch weitere Fragen? Dann schicken Sie doch einfach eine Mail an presse@piraten-thueringen.de oder rufen uns an unter 0361-6606878.</p>
 				<h2>Service f&uuml;r Journalisten</h2>
 				<p>Sie haben Fragen zum Landesparteitag, zur Piratenpartei, dem Landesverband Th&uuml;ringen oder ben&ouml;tigen Bildmaterial? Dann schauen Sie doch in unser <a href="http://medienarchiv.piraten-thueringen.de/" class="p">Medienarchiv</a> oder kontaktieren Sie uns einfach telefonisch oder per E-Mail:</p>
 				<p>
