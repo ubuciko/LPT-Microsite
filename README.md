7a4ea6b333686231bcc6ce203352c5c6d6eb9278
Horay Narea <HorayNarea@googlemail.com>
2012-09-19 12:47:36 +0200

    Twidget, README, Kleinigkeiten
    Neues README (Markdown <3), Twitter-Widget auf Startseite, ein paar
    kleine Änderungen an index.php und style.css

---
+++ README.md
@@ -0,0 +1,8 @@
+Basierend auf [ubuciko/LPT-Microsite](https://github.com/ubuciko/LPT-Microsite) verwendet unter [CC-BY-NC 3.0](http://creativecommons.org/licenses/by-nc/3.0/)
+
+**Änderungen gegenüber dem Original:**
+* Stimmzettel-Bild in index.php ausgetauscht durch Twitter-Widget
+* 
+
+**Twitter-Widget anpassen**
+Um das Twitter-Widget anzupassen einfach die entsprechenden Daten in *twitdget.js* bearbeiten
--- document_root/index.php
+++ document_root/index.php
@@ -1,29 +1,32 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<!--  Piratenpartei LV Thüringen – LPT-Microsite -->
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="de">
 	<head>
-		<title>Landesparteitag der PIRATEN Th&uuml;ringen 2012</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta name="description" content="Diese Seite basiert auf https://github.com/ubuciko/LPT-Microsite verwendet unter CC-BY-NC 3.0 – http://creativecommons.org/licenses/by-nc/3.0/"/>
+
+		<title>Landesparteitag der PIRATEN Thüringen 2012</title>
 		<link rel="stylesheet" type="text/css" href="style.css" />
-		<script charset="utf-8" src="http://widgets.twimg.com/j/2/widget.js"></script>
+		<script type="text/javascript" src="http://widgets.twimg.com/j/2/widget.js"></script>
 	</head>
+
 	<body>
 		<div id="wrapper">
 			<div id="header">
 				<img src="logo.png" class="logo" />
-				<h1>#lptth12</h1>
-				<p>Der Landesparteitag der PIRATEN Th&uuml;ringen</p>
+				<h1>#lptavth</h1>
+				<p>Landesparteitag und Aufstellungsversammlung der PIRATEN Thüringen</p>
 			</div>
 			<ul class="nav">
 				<li class="active"><a href="index.php">Hauptseite</a></li>
 				<li><a href="anmeldung.php">Anmeldung</a></li>
 				<li><a href="anfahrt.php">Anfahrt</a></li>
-				<li><a href="verpflegung.php">Verpflegung &amp; &Uuml;bernachtung</a></li>
+				<li><a href="verpflegung.php">Verpflegung & übernachtung</a></li>
 				<li><a href="presse.php">Presse</a></li>
-				<li><a href="impressum.php">Impressum &amp; Kontakt</a></li>
+				<li><a href="impressum.php">Impressum & Kontakt</a></li>
 			</ul>
 			<div id="content">
-				<img src="stimmkarte.jpg" class="floatr" />
+				<!-- <img src="stimmkarte.jpg" class="floatr" /> -->
+				<script type="text/javascript" src="twidget.js"></script>
 				<h2>Eckdaten</h2>
 				
 				<p>
@@ -33,37 +36,37 @@
 				</p>
 				<h3>Veranstaltungsort:</h3>
 				<p>
-					<strong>B&uuml;rgerhaus Erfurt-Schmira</strong><br />
+					<strong>Bürgerhaus Erfurt-Schmira</strong><br />
 					Seestra&szlig;e 18<br />
 					99094 Erfurt-Schmira<br />
 				</p>
 				<h2>Was passiert auf dem Landesparteitag?</h2>
 				<p>Auf dem Parteitag werden der Landesvorstand, das Schiedsgericht sowie
-				die Kassenpr&uuml;fer gew&auml;hlt. Zudem k&ouml;nnen Programm&auml;nderungsantr&auml;ge,
+				die Kassenprüfer gew&auml;hlt. Zudem k&ouml;nnen Programm&auml;nderungsantr&auml;ge,
 				Satzungs&auml;nderungsantr&auml;ge, sonstige Antr&auml;ge und Positionspapiere
 				eingereicht und abgestimmt werden.</p>
 
-				<p>Wenn Du f&uuml;r ein Amt kandidieren m&ouml;chtest, so trag Dich bitte in die
+				<p>Wenn Du für ein Amt kandidieren m&ouml;chtest, so trag Dich bitte in die
 				<a href="http://wiki.piraten-thueringen.de/TH:Landesparteitag_2012.1/Kandidatenliste" class="p">Kandidatenliste im Wiki</a> ein. Dort
-				kannst Du Dich auch &uuml;ber die bisherigen Kandidaten informieren, um Dir
+				kannst Du Dich auch über die bisherigen Kandidaten informieren, um Dir
 				schon vor dem Parteitag ein Bild von ihnen zu machen und um Dich selbst
 				vorzustellen.</p>
 				
 				<h2>Vorl&auml;ufige Tagesordnung</h2>
 				<ul>
-					<li>TOP 1: Begr&uuml;&szlig;ung durch den Landesvorstand </li>
+					<li>TOP 1: Begrü&szlig;ung durch den Landesvorstand </li>
 					<li>TOP 2: Wahl der Versammlungs&auml;mter; Zulassung von Presse, G&auml;sten, Ton- und Filmaufnahmen und evtl. Gastreden </li>
 					<li>TOP 3: Beschluss der Tages- und Gesch&auml;ftsordnung </li>
-					<li>TOP 4: Wahl der Rechnungspr&uuml;fer </li>
-					<li>TOP 5: Satzungs&auml;nderungsantr&auml;ge betreffend den Landesvorstand, den Landesparteitag, das Schiedsgericht oder die Einf&uuml;hrung neuer Parteiorgane </li>
+					<li>TOP 4: Wahl der Rechnungsprüfer </li>
+					<li>TOP 5: Satzungs&auml;nderungsantr&auml;ge betreffend den Landesvorstand, den Landesparteitag, das Schiedsgericht oder die Einführung neuer Parteiorgane </li>
 					<li>TOP 6: T&auml;tigkeitsberichte des Vorstands </li>
-					<li>TOP 7: Bericht der Rechnungspr&uuml;fer, Entlastung des Vorstands </li>
-					<li>TOP 8: Wahl des Landesvorstands (Vorsitzender, stellvertretender Vorsitzender, Schatzmeister, Generalsekret&auml;r, politischer Gesch&auml;ftsf&uuml;hrer, Beisitzer sowie eventuell eingef&uuml;hrte weitere &Auml;mter) </li>
+					<li>TOP 7: Bericht der Rechnungsprüfer, Entlastung des Vorstands </li>
+					<li>TOP 8: Wahl des Landesvorstands (Vorsitzender, stellvertretender Vorsitzender, Schatzmeister, Generalsekret&auml;r, politischer Gesch&auml;ftsführer, Beisitzer sowie eventuell eingeführte weitere &Auml;mter) </li>
 					<li>TOP 9: Arbeitsbericht des Landesschiedsgerichts </li>
 					<li>TOP 9a: Wahl des Landesschiedsgerichts und der Ersatzschiedsrichter </li>
-					<li>TOP 10: Wahl der Kassenpr&uuml;fer </li>
+					<li>TOP 10: Wahl der Kassenprüfer </li>
 					<li>TOP 11: Wahl der Mitglieder des Finanzrates </li>
-					<li>TOP 12: Wahl der Moderatoren der Th&uuml;ringer Mailingliste </li>
+					<li>TOP 12: Wahl der Moderatoren der Thüringer Mailingliste </li>
 					<li>TOP 13: Satzungs&auml;nderungsantr&auml;ge </li>
 					<li>TOP 14: Programm&auml;nderungsantr&auml;ge </li>
 					<li>TOP 15: Positionspapiere und sonstige Antr&auml;ge </li>
